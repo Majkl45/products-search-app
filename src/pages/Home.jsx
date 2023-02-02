33fd2c16e5f02ca190c8d9e384025b67cbf9b2af
Michael Rohm <michaelrohm24@gmail.com>
2023-02-02 01:48:50 +0100

    Remove pages

--- src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-export function Home() {
-  return (
-    <div className="container">
-      <h2>Produkty</h2>  
-    </div>
-  )
-}
--- src/pages/Parts.jsx
+++
@@ -1,7 +0,0 @@
-export function Parts() {
-  return (
-    <div className="container">
-      <h2>Náhradní díly</h2>
-    </div>
-  )
-}
